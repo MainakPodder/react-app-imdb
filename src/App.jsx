9373f90aaf0eb1a42057e2e536e61f1e74af6727
Mainak Podder <mainakshuvo7@gmail.com>
2022-02-17 15:39:13 +0530

    Key

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import './index.css'
 import Cards from './Cards'
 import movieData from './Data'
-import './index.css'
 const App = () => {
     return (
         <>
             <h1 className="headingStyle">BEST BOLLYWOOD MOVIES 2022 - IMDb</h1>
             {movieData.map((value) => {
+                console.log(value);
                 return (
                     <Cards
+                        key={value.key}
                         name={value.name}
                         img={value.img}
                         content={value.content}
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,35 +1,40 @@
 const movieData = [
     {
-        name : "'83 (2021)",
-        img : "https://m.media-amazon.com/images/M/MV5BNTc0MWIzMjYtMDg4Mi00YmM3LWIxNGUtYjQ5NzBlZTM3YWI2XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR13,0,140,209_AL_.jpg",
-        content : "Biography, Drama, History",
-        rating : "7.4",
-        links : "https://www.imdb.com/title/tt7518786/?ref_=ttls_li_tt"
+        key: 1,
+        name: "'83 (2021)",
+        img: "https://m.media-amazon.com/images/M/MV5BNTc0MWIzMjYtMDg4Mi00YmM3LWIxNGUtYjQ5NzBlZTM3YWI2XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR13,0,140,209_AL_.jpg",
+        content: "Biography, Drama, History",
+        rating: "7.4",
+        links: "https://www.imdb.com/title/tt7518786/?ref_=ttls_li_tt"
     },
     {
-        name : "Shamshera",
-        img : "https://m.media-amazon.com/images/M/MV5BM2Y5MWRkNjQtOGM3NS00NzBmLTlkYjAtOTgyOGNlNGEwNWZlXkEyXkFqcGdeQXVyNTkzNDQ4ODc@._V1_UY209_CR8,0,140,209_AL_.jpg",
-        content : "Adventure, Action",
-        rating : "---X---",
-        links : "https://www.imdb.com/title/tt8426926/?ref_=ttls_li_tt"
-    },{
-        name : "Bhool Bhulaiyaa 2",
-        img : "https://m.media-amazon.com/images/M/MV5BYjczMWFhMjUtMzM2YS00MzgxLTk2NzktOTI0OWI4NzQ5N2RlXkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_UY209_CR34,0,140,209_AL_.jpg",
-        content : "Comedy, Horror",
-        rating : "---X---",
-        links : "https://www.imdb.com/title/tt6455162/?ref_=ttls_li_tt"
-    },{
-        name : "Gangubai Kathiawadi",
-        img : "https://m.media-amazon.com/images/M/MV5BNmJmYThmYzgtY2E2My00YmI2LWE5ZTctMWViMzlmNzZhMjlkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_UY209_CR0,0,140,209_AL_.jpg",
-        content : "Biography, Crime, Drama",
-        rating : "---X---",
-        links : "https://www.imdb.com/title/tt10083340/?ref_=ttls_li_tt"
-    },{
-        name : "Jersey",
-        img : "https://m.media-amazon.com/images/M/MV5BYzRjNzQ0YzYtYWRhZC00ZjgwLWFjNTktZjk3N2MwZjJhNTgxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR2,0,140,209_AL_.jpg",
-        content : "Drama, Sport",
-        rating : "---X---",
-        links : "https://www.imdb.com/title/tt5301942/?ref_=ttls_li_tt"    
+        key: 2,
+        name: "Shamshera",
+        img: "https://m.media-amazon.com/images/M/MV5BM2Y5MWRkNjQtOGM3NS00NzBmLTlkYjAtOTgyOGNlNGEwNWZlXkEyXkFqcGdeQXVyNTkzNDQ4ODc@._V1_UY209_CR8,0,140,209_AL_.jpg",
+        content: "Adventure, Action",
+        rating: "---X---",
+        links: "https://www.imdb.com/title/tt8426926/?ref_=ttls_li_tt"
+    }, {
+        key: 3,
+        name: "Bhool Bhulaiyaa 2",
+        img: "https://m.media-amazon.com/images/M/MV5BYjczMWFhMjUtMzM2YS00MzgxLTk2NzktOTI0OWI4NzQ5N2RlXkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_UY209_CR34,0,140,209_AL_.jpg",
+        content: "Comedy, Horror",
+        rating: "---X---",
+        links: "https://www.imdb.com/title/tt6455162/?ref_=ttls_li_tt"
+    }, {
+        key: 4,
+        name: "Gangubai Kathiawadi",
+        img: "https://m.media-amazon.com/images/M/MV5BNmJmYThmYzgtY2E2My00YmI2LWE5ZTctMWViMzlmNzZhMjlkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_UY209_CR0,0,140,209_AL_.jpg",
+        content: "Biography, Crime, Drama",
+        rating: "---X---",
+        links: "https://www.imdb.com/title/tt10083340/?ref_=ttls_li_tt"
+    }, {
+        key: 5,
+        name: "Jersey",
+        img: "https://m.media-amazon.com/images/M/MV5BYzRjNzQ0YzYtYWRhZC00ZjgwLWFjNTktZjk3N2MwZjJhNTgxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR2,0,140,209_AL_.jpg",
+        content: "Drama, Sport",
+        rating: "---X---",
+        links: "https://www.imdb.com/title/tt5301942/?ref_=ttls_li_tt"
     }
 ]
 export default movieData;
